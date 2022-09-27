30c17dc706f521ed6c9d89b1be8ee5a39df4aaf9
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-09-27 14:08:09 +0200

    Creates full page scroll

--- vue/first-vue-project/src/App.vue
+++ vue/first-vue-project/src/App.vue
@@ -1,15 +1,83 @@
 <script>
   import HomePage from './components/HomePage.vue'
   export default {
+    data(){
+      return{
+        inMove:false,
+        inMoveDelay:400,
+        activeSection:0,
+        offsets:[],
+        touchStartY:0
+      }
+    },
     components:{
       HomePage
+    },
+    methods:{
+      calculateSectionOffset(){
+      let sections = document.getElementsByClassName('fullpage');
+      let numberOfSections = sections.length;
+
+      for(let i = 0; i<numberOfSections; i++ ){
+        this.offsets.push(sections[i].offsetTop)
+      }
+      },
+      scrollToSection(id, force = false) {
+        if (this.inMove && !force) return false;
+        this.activeSection = id;
+        this.inMove = true;
+        document.getElementsByTagName('section')[id].scrollIntoView({
+            behavior: 'smooth'
+        });
+        setTimeout(() => {
+            this.inMove = false;
+        }, this.inMoveDelay);
+      },
+      moveDown() {
+        this.inMove = true;
+        this.activeSection--;
+        if (this.activeSection < 0) this.activeSection = this.offsets.length - 1;
+        this.scrollToSection(this.activeSection, true);
+      },
+      moveUp() {
+        this.inMove = true;
+        this.activeSection++;
+        if (this.activeSection > this.offsets.length - 1) this.activeSection = 0;
+        this.scrollToSection(this.activeSection, true);
+      }
+    },
+    
+    mounted(){
+      this.calculateSectionOffset();
+      window.addEventListener('mousewheel',(e)=>{
+        let delta = e.wheelDelta;
+        console.log(delta);
+        if(delta>100 && !this.inMove){
+          this.moveUp();
+          console.log("up")
+        } else if(delta<-100 && !this.inMove){
+          console.log("down")
+          this.moveDown();
+        }
+        e.preventDefault();
+        return false;
+      },{passive:false})
+    },
+    destroyed(){
+      window.removeEventListener('mousewheel',this.handleMouseWheel,{passive:false});
     }
-  }
+  };
+  
   </script>
   
   <template>
     <main>
-      <HomePage/>
+      <section class="fullpage">
+        <HomePage/>
+      </section>
+      <section class="fullpage">
+        <FriendPage/>
+      </section>
     </main>
   </template>
 
@@ -23,6 +91,13 @@
 
     .text{
       font-size: 2em;
+    }
+
+    .fullpage{
+      width: 100vw;
+      height: 100vh;
+      max-width: 100%;
+      margin: 0;
     }
 
   </style>
--- vue/first-vue-project/src/components/HomePage.vue
+++ vue/first-vue-project/src/components/HomePage.vue
@@ -1,22 +1,87 @@
 <script>
+  import anime from 'animejs/lib/anime.es.js'
+    export default {
+        methods:{
+            bounce(){
+                anime(
+                    {
+                        targets:'.square',
+                        translateY: -50,
+                        duration: 300,
+                        direction: 'alternate',
+                        loop: true,
+                        easing: 'easeInCubic',
+                        scaleX: {
+                            value: 1.05,
+                            duration: 100,
+                            delay: 268
+	                    }
+                    }
+                );
+                anime(
+                    {
+                        targets:'.happyText',
+                        opacity: [0,1],
 
+                    }
+                )
+            }
+        },
+        mounted(){
+            let timeline = anime.timeline();
+            timeline.add(
+                {
+                    targets:'.square',
+                    opacity: [0,1],
+                    duration: 2000,
+                }
+            )
+            .add(
+                {
+                    targets:'.letter',
+                    translateY:[-100,0],
+                    opacity: [0,1],
+                    duration: 500,
+                    easing: "easeOutExpo",
+                    delay:(el,i)=>100*i,
+                },'-=1000')
+        }
+    }
 </script>
 
 <template>
     <div class="container homepageContainer">
         <div class="squareContainer">
-            <div class="square"></div>
+            <div class="square" @mouseover="bounce"></div>
         </div>
         <div class="textContainer">
-            <p class="text">This is square.</p>
+            <p class="text" id="introText">
+            <span class="letter">T</span>
+            <span class="letter">h</span>
+            <span class="letter">i</span>
+            <span class="letter">s </span> 
+            <span class="letter">&nbsp;</span>
+            <span class="letter">i</span>
+            <span class="letter">s </span> 
+            <span class="letter">&nbsp;</span>
+            <span class="letter">s</span>
+            <span class="letter">q</span>
+            <span class="letter">u</span>
+            <span class="letter">a</span>
+            <span class="letter">r</span>
+            <span class="letter">e</span>
+            <span class="letter">.</span>
+        </p>
+        <p class="happyText">Square is happy!</p>
         </div>
+        
     </div>
 </template>
 
 <style>
     .container{
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
 
         display: flex;
         justify-content: center;
@@ -28,6 +93,20 @@
         width: 200px;
         height: 200px;
         background-color: red;
+        opacity: 0;
+        box-shadow: 0 0 2px 5px red;
+    }
+
+    .text{
+        margin: 0;
+    }
+
+    .happyText{
+        opacity: 0;
+    }
+
+    .letter{
+        display: inline-block;
     }
 
 
