b35ec5b34dcaecd803b25c85e68275e9fe879144
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-09-27 10:20:30 +0200

    New project

--- vue/first-vue-project/src/App.vue
+++ vue/first-vue-project/src/App.vue
@@ -1,26 +1,41 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+import { RouterLink, RouterView } from "vue-router";
+let id = 0;
+  export default{
+    data(){
+      return {
+        newTodo:'',
+        todos:[
+          {id:id++,text:'Learn Vue'},
+          {id:id++,text:'Learn anime.js'},
+        ]
+      }
+    },
+    methods: {
+      addNewTodo(){
+        this.todos.push({id:id++, text:this.newTodo});
+        this.newTodo = '';
+      },
+      removeTodo(todo){
+        this.todos = this.todos.filter((t)=>t !==todo)
+      }
+    }
+  }
 </script>
 
+<template>
+  <form @submit.prevent="addNewTodo">
+    <input type="text" v-model="newTodo">
+    <button>Add task</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{todo.text}}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   
 </style>
