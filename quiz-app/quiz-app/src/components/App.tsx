cdec178c2f182b54589401f6bbc8e8f013586ffd
Benoitdo <benoit.dossoine@student.ehb.be>
2022-10-03 11:54:46 +0200

    Change to typescript + refactoring

---
+++ quiz-app/quiz-app/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/App.css';
+import Homepage from './Homepage'
+function App() {
+  return (
+    <Homepage />
+  );
+}
+
+export default App;
---
+++ quiz-app/quiz-app/src/components/Homepage.tsx
@@ -0,0 +1,9 @@
+function Homepage(){
+    return(
+        <div id="homepageContainer">
+            <p>Welcome!</p>
+        </div>
+    )
+}
+
+export default Homepage
