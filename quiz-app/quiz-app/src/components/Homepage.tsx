5d98c9041fd7562a6a1cfb834f57ce87229688f6
Benoitdo <benoit.dossoine@student.ehb.be>
2022-10-03 13:23:01 +0200

    Simple homepage with two buttons

--- quiz-app/quiz-app/src/components/Homepage.tsx
+++ quiz-app/quiz-app/src/components/Homepage.tsx
@@ -1,7 +1,14 @@
+import HomepageButton from "./HomepageButton"
+
 function Homepage(){
     return(
         <div id="homepageContainer">
-            <p>Welcome!</p>
+            <p className="introText">Welcome!</p>
+            <p className="introText">Choose your player status:</p>
+            <div className="homepageBtnContainer">
+                <HomepageButton content='player'/>
+                <HomepageButton content='quiz master'/>
+            </div>
         </div>
     )
 }
---
+++ quiz-app/quiz-app/src/components/HomepageButton.tsx
@@ -0,0 +1,7 @@
+function HomepageButton(props:any){
+    return(
+        <button className="homepageBtn">{props.content}</button>
+    )
+}
+
+export default HomepageButton
