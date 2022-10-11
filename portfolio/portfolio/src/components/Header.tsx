654c0e0a1fb53185117fa7c918231748c4370b87
Benoitdo <benoit.dossoine@student.ehb.be>
2022-10-11 15:07:16 +0200

    feat(header): Add header with hover effects

---
+++ portfolio/portfolio/src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <ul className="nav">
+                <li className="navElement">Home</li>
+                <li className="navElement">Work</li>
+                <li className="navElement">About</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
