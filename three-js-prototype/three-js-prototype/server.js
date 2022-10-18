9d0659f0c4e5ceff36f8d14a5ac952dbe7c257ad
Benoitdo <benoit.dossoine@student.ehb.be>
2022-10-18 17:29:19 +0200

    feat(upload): Possible to upload files to models folder

---
+++ three-js-prototype/three-js-prototype/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const cors = require('cors');
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'public/models')
+    },
+    filename: function(req,file,cb){
+        cb(null,`${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload = multer({storage:storage}).single('file');
+
+app.post('/upload',function(req,res){
+    upload(req,res,function(err){
+        if(err){
+            return res.status(500).json(err)
+        }
+        return res.status(200).send(req.file);
+    })
+})
+
+app.listen(8000,()=>console.log('App running on port 8000'));
--- three-js-prototype/three-js-prototype/src/components/Upload.tsx
+++ three-js-prototype/three-js-prototype/src/components/Upload.tsx
@@ -1,7 +1,29 @@
+import {useState} from 'react';
+import axios from 'axios';
+
 function Upload(){
+    const [file,setFile] = useState('')
+    const onChangeHandler = (event:any) =>{
+        setFile(event.target.files[0]);
+    }
+
+    const onClickHandler = ()=>{
+        const data = new FormData();
+        data.append('file',file);
+    
+        axios.post('http://localhost:8000/upload',data,{
+
+        })
+        .then(response =>{
+            console.log(response.statusText);
+        })
+    }
+
     return(
         <form>
-            
+            <input type="file" name="file" id="fileUpload"
+            onChange={onChangeHandler} />
+            <button type="button" onClick={onClickHandler}>Upload model</button>
         </form>
     )
 }
