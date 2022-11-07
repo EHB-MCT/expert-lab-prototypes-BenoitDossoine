3d528cc87b12f1ee9c700260fa79014560d1c683
Benoitdo <benoit.dossoine@student.ehb.be>
2022-11-07 14:09:57 +0100

    Rework

--- portfolio/portfolio/src/components/App.tsx
+++ portfolio/portfolio/src/components/App.tsx
@@ -1,17 +1,11 @@
-import Header from "./Header";
 import {Routes, Route, useLocation} from "react-router-dom"
-import Homepage from "./Homepage";
 import {AnimatePresence} from "framer-motion";
 
 function App() {
   const location = useLocation();
   return (
     <div className="App">
-      <AnimatePresence exitBeforeEnter initial={false}>
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Homepage/>}></Route>
-        </Routes>
-      </AnimatePresence>
+      
     </div>
   );
 }
