a2f75f13bf794cd18f0686bce618823de960d2a3
Benoitdo <benoit.dossoine@student.ehb.be>
2022-11-29 13:12:06 +0100

    positional audio: experience fix

--- portfolio/portfolio/src/components/PositionalAudio.tsx
+++ portfolio/portfolio/src/components/PositionalAudio.tsx
@@ -1,6 +1,7 @@
 import {useRef, useState, useEffect, useLayoutEffect} from 'react';
 import * as THREE from 'three';
 
+
 import {useLoader,useThree} from '@react-three/fiber';
 
 import {gsap} from 'gsap';
@@ -14,13 +15,15 @@ function PositionalAudio(props:any){
     const [listener] = useState(()=>new THREE.AudioListener());
     const buffer = useLoader(THREE.AudioLoader,props.url);
 
-
-
     useEffect(():any=>{
         if(audio.current){
             audio.current.setBuffer(buffer);
-            audio.current.setRefDistance(0.01);
+            audio.current.setRefDistance(0.1);
             audio.current.setLoop(true);
+            audio.current.panner.coneInnerAngle = 180;
+            audio.current.panner.coneOuterAngle = 230;
+            audio.current.panner.coneOuterGain = 0;
+            console.log(audio.current.panner)
             camera.add(listener);
             return () => camera.remove(listener);
         }
